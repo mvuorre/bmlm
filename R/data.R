f783082ead872362ca3272f254755917dea65386
Mvuorre <mv2521@columbia.edu>
2017-02-09 14:31:22 -0500

    Add example data used in manuscript (Metcalfe, Eich, & Castel, 2010)

--- R/data.R
+++ R/data.R
@@ -24,3 +24,28 @@
 #'
 #' @source \url{http://www.intensivelongitudinal.com/datasets.html}
 "BLch9"
+
+#' Judgments of performance in a video game
+#'
+#' @description Data from an experiment where participants rated their
+#' performance in a video game in two conditions.
+#' (Experiment 1 in Metcalfe, Eich, & Castel, 2010;
+#' \url{http://www.sciencedirect.com/science/article/pii/S0010027710001113}).
+#'
+#' @docType data
+#'
+#' @usage data(MEC2010)
+#'
+#' @format A data frame with 344 rows and 4 variables:
+#' \describe{
+#'   \item{subj}{Subject id number.}
+#'   \item{lag}{Lag condition (0 = no lag, 1 = 250ms lag).}
+#'   \item{hr}{Hit rate.}
+#'   \item{jop}{Judgment of Performance.}
+#' }
+#'
+#' @source Metcalfe, J., Eich, T. S., & Castel, A. D. (2010).
+#' Metacognition of agency across the lifespan.
+#' Cognition, 116(2), 267–282.
+#' \url{https://doi.org/10.1016/j.cognition.2010.05.009}
+"MEC2010"
